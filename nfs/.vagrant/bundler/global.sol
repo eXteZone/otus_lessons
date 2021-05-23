cfcc0af58c2d9ff27fb6a395c4fddba4ff52c3f8
Aleksey Gaidukov <extezone@gmail.com>
2021-05-23 15:48:17 +0300

    remove .vagrant

--- nfs/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["net-ssh",[">= 2.6.5"]],["net-scp",["~> 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.7"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.29.0"]]],"checksum":"3a35bdb4c9217012f8bffcd8f2bdc7cab21808ac85a404705810a951cc3d2a79","vagrant_version":"2.2.15"}
